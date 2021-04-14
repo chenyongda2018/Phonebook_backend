c054c52687bdad83f6ef2f83410059a73e37173b
chenyongda <chenyongda3@xiaomi.com>
2021-04-14 10:27:00 +0800

    test

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ morgan.token('body',(req,rsp) => JSON.stringify(req.body))
 }
 
 const app = express();
-app.use(express.static('build'))
 app.use(cors());
+app.use(express.static('build'))
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(unknownEndpoint);
