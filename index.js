ae8a679be64c0a2935b05b188fbc2506c6e8b252
chenyongda <chenyongda3@xiaomi.com>
2021-04-15 17:36:26 +0800

    add eslint plugin

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 require('dotenv').config()
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const Person = require('./models/person')
 
 morgan.token('body', (req, rsp) => JSON.stringify(req.body))
 /**
  * 对未知页面处理
- * @param {*} req 
- * @param {*} rsp 
+ * @param {*} req
+ * @param {*} rsp
  */
 const unknownEndpoint = (req, rsp, next) => {
-    console.log('path: ', req.path);
+    console.log('path: ', req.path)
     if (!req.path.startsWith('/api/')) {
-        return rsp.status(404).send({ error: 'uknown error.' });
+        return rsp.status(404).send({ error: 'uknown error.' })
     }
-    next();
+    next()
 }
 
 const errorHandler = (error, req, rsp, next) => {
-    console.error(error);
+    console.error(error)
     if (error.name === 'CastError' && error.kind === 'ObjectId') {
         return rsp.status(400).send({ error: 'malformatted id' })
     }
     if (error.name === 'ValidationError' ) {
-        return rsp.status(400).json({ error: error.message });
+        return rsp.status(400).json({ error: error.message })
     }
     next(error)
 }
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 app.use(express.static('build'))
-app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(unknownEndpoint);
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(unknownEndpoint)
 
 /**
  * 获取所有联系人
@@ -44,7 +44,7 @@ app.get('/api/persons', (req, rsp) => {
         if (!result) {
             return rsp.status(400).json({ error: 'There is no any data.' })
         }
-        return rsp.status(200).json(result);
+        return rsp.status(200).json(result)
     })
 })
 
@@ -52,9 +52,9 @@ app.get('/api/persons', (req, rsp) => {
  * 获取总体描述
  */
 app.get('/api/infos', (req, rsp) => {
-    const curTime = new Date();
+    const curTime = new Date()
     Person.find({}).then(result => {
-        console.log('person infos:', result);
+        console.log('person infos:', result)
         rsp.send(
             `Phonebook has info for ${result.length} people.<br/>
             ${curTime}`
@@ -66,7 +66,7 @@ app.get('/api/infos', (req, rsp) => {
  * 根据id查询
  */
 app.get('/api/persons/:id', (req, rsp, next) => {
-    console.log('get person id: ', req.params.id);
+    console.log('get person id: ', req.params.id)
     Person
         .findById(req.params.id)
         .then(person => {
@@ -83,12 +83,12 @@ app.get('/api/persons/:id', (req, rsp, next) => {
  * 创建新的记录
  */
 app.post('/api/persons', (req, rsp, next) => {
-    const body = req.body;
+    const body = req.body
     if (!body.name) {
-        return rsp.status(400).json({ error: 'Name must not be empty' });
+        return rsp.status(400).json({ error: 'Name must not be empty' })
     }
     if (!body.number) {
-        return rsp.status(400).json({ error: 'Number must not be empty' });
+        return rsp.status(400).json({ error: 'Number must not be empty' })
     }
     const person = new Person({
         name: body.name,
@@ -96,7 +96,7 @@ app.post('/api/persons', (req, rsp, next) => {
     })
     person.save()
         .then(result => {
-            rsp.json(person);
+            rsp.json(result)
         })
         .catch(error => { next(error) })
 })
@@ -119,20 +119,21 @@ app.put('/api/persons/:id', (req, rsp, next) => {
         .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, rsp) => {
+app.delete('/api/persons/:id', (req, rsp,next) => {
     Person
         .findByIdAndRemove(req.params.id)
         .then(result => {
-            return rsp.status(204).end();
+            console.log(result)
+            return rsp.status(204).end()
         })
         .catch(error => {
             next(error)
         })
 })
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log('Server is listenning port:' + PORT);
+    console.log('Server is listenning port:' + PORT)
 })
--- models/person.js
+++ models/person.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGO_URI;
+const url = process.env.MONGO_URI
 
-console.log('Connect to: ',url);
+console.log('Connect to: ',url)
 
 mongoose.connect(url, {
     useNewUrlParser: true,
@@ -11,10 +11,10 @@ mongoose.connect(url, {
     useFindAndModify: false,
     useCreateIndex: false
 }).then(result => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB')
 }).catch(error => {
-    console.log('Error connect to MongoDB',error.message);
-});
+    console.log('Error connect to MongoDB',error.message)
+})
 
 const personSchema = new mongoose.Schema({
     name: {
@@ -29,14 +29,14 @@ const personSchema = new mongoose.Schema({
         minLength: 5,
         required: true
     }
-});
-personSchema.plugin(uniqueValidator);
+})
+personSchema.plugin(uniqueValidator)
 //格式化mongoose返回的对象
 personSchema.set('toJSON',{
     transform:(document,returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
 })
 
