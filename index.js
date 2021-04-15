8c331a867c4706fe32b9f26a176feb19a3f6cb6e
chenyongda <chenyongda3@xiaomi.com>
2021-04-15 14:23:17 +0800

    fix function name

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(cors());
 app.use(express.static('build'))
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(upLog);
+app.use(ipLog);
 app.use(unknownEndpoint);
 
 /**
